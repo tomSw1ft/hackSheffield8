228c298e41d7fc9c5a5c18395f10a206c3e9eedf
Jakub Mikołajczak <jmikolajczak1@sheffield.ac.uk>
2023-11-11 14:41:34 +0000

    read csv

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import numpy as np
 
 def main():
-    print()
-    
+    FILE_PATH = "data.csv"
+    df = pd.read_csv(FILE_PATH)
+    data = df.values
+    temp = data[:, 8]
+    print(np.nanmean(temp))
+
+
 if __name__ == "__main__":
     main()
